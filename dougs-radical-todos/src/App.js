38a41bbc1e483e73169632aba720defb901fcf95
DouglasMacKrell <douglasmackrell@pursuit.org>
2019-12-09 21:33:15 -0500

    First commit

---
+++ dougs-radical-todos/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './App.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+class App extends Component {
+  notify = () => toast.info('🦄 Wow so easy!', {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true
+    });
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.notify}>Notify !</button>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnVisibilityChange
+          draggable
+          pauseOnHover
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
